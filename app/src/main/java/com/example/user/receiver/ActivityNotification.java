177e20144d131dbb3dafa1b8bb81853e50ed74cb
fcu-d0449763 <d0449763@fcu.edu.tw>
2017-04-18 21:09:31 +0800

    update
    Signed-off-by: fcu-d0449763 <d0449763@fcu.edu.tw>

---
+++ app/src/main/java/com/example/user/receiver/ActivityNotification.java
@@ -0,0 +1,16 @@
+package com.example.user.receiver;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+public class ActivityNotification extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(ActivityNotification.this);
+        tv.setText("ActivityNotification!");
+        setContentView(tv);
+    }
+}
+
---
+++ app/src/main/java/com/example/user/receiver/BR_Notification.java
@@ -0,0 +1,52 @@
+package com.example.user.receiver;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class BR_Notification extends BroadcastReceiver{
+
+    static int id = 70000;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String msg = intent.getStringExtra("KEY_MSG");
+
+        Intent newintent = new Intent();
+        newintent.setClass(context, ActivityNotification.class);
+        PendingIntent pendingIntent = PendingIntent.
+                getActivity(context, 0, newintent, 0);
+
+        Notification notify = null;
+        notify = newNotification(context, pendingIntent, "(New) Broadcast is received.", msg);
+
+        NotificationManager notificationManager =
+                (NotificationManager)context.
+                        getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(id++, notify);
+    }
+
+    @SuppressLint("NewApi")
+    private Notification newNotification(
+            Context context, PendingIntent pi,
+            String title, String msg) {
+
+        Notification.Builder builder =
+                new Notification.Builder(context);
+        builder.setContentTitle(title);
+        builder.setContentText(msg);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pi);
+        builder.setTicker(msg);
+        builder.setWhen(System.currentTimeMillis());
+        Notification notify = builder.build();
+        return notify;
+    }
+}
